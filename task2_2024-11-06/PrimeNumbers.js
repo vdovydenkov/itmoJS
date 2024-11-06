1f9e99ab7614a86f0580bcb5f4fbc6d1907b41b1
VDovydenkov <dov@mlbox.ru>
2024-11-06 23:25:08 +0300

    Added some comments and removed some figure brackets.

--- task2_2024-11-06/PrimeNumbers.js
+++ task2_2024-11-06/PrimeNumbers.js
@@ -1,23 +1,20 @@
+// Задание №2. 06.11.2024.
+// Определить количество простых чисел в диапазоне от 2 до заданного значения.
+
 function isPrime(n) {
-    if (!Number.isInteger(n)) {
+    if (!Number.isInteger(n)) // Если аргумент не целое число
         return false;
-    }
-    if (n < 2) {
+    if (n < 2)
         return false;
-    }
-    if (n === 2) {
+    if (n === 2) // Исключаем двойку, чтобы не перебирать четные числа
         return true;
-    }
-    if (n % 2 === 0) {
+    if (n % 2 === 0) // Исключаем из оценки все четные числа
         return false;
-    }
     // Цикл по нечетным числам, до корня из числа, с шагом 2.
-    for (let i = 3; i <= Math.sqrt(n); i += 2) {
+    for (let i = 3; i <= Math.sqrt(n); i += 2)
         // Если остаток от деления дал ноль - число не простое
-        if (n % i === 0) {
+        if (n % i === 0)
             return false;
-        }
-    }
     return true;
 }
 
@@ -25,13 +22,12 @@ const upperBound = Number(process.argv[2]);
 if (!Number.isInteger(upperBound) || upperBound < 2) {
     console.error("The upper bound is incorrect.");
 } else {
-    let counter = 1;
+    let counter = 1; // Как минимум 2 - простое число
     if (upperBound > 2) {
-        for (let i = 3; i <= upperBound; i += 2) {
-            if (isPrime(i)) {
+        // По нечетным числам, до верхней границы, с шагом 2.
+        for (let i = 3; i <= upperBound; i += 2)
+            if (isPrime(i))
                 counter++;
-            }
-        }
     }
     console.log(`The number of prime numbers in the range from 2 to ${upperBound} is ${counter}.`)
 }
