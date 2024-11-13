7ff1b9f4987816e0ef8a40266697a89d5e2e8b06
VDovydenkov <dov@mlbox.ru>
2024-11-13 11:55:07 +0300

    Added the Cramer test.

---
+++ task2_2024-11-06/cramer_test.mjs
@@ -0,0 +1,5 @@
+﻿import { execSync } from 'node:child_process';
+
+const result = String(execSync('node cramer_v2.js 2 3 18 4 -5 -8')).trim().replace('/\n/g', ' ');
+
+console.assert(result === '3 4', 'НЕУДАЧА!');
