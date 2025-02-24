a1922c9939ad2303e0a2b46315c375dcdeada82b
VDovydenkov <dov@mlbox.ru>
2025-02-24 17:50:27 +0300

    Added a mitata benchmark.

---
+++ task2_2025-02-15/hash_benchmark.mjs
@@ -0,0 +1,25 @@
+﻿import { bench, run } from 'mitata';
+import { hash, createHash } from 'node:crypto';
+
+const data = 'Hello, World!';
+const algorithm = 'sha256';
+
+// Функция с использованием crypto.hash (Node.js 20+)
+async function hashFunction() {
+  await hash(algorithm, Buffer.from(data));
+}
+
+// Функция с использованием createHash
+function createHashFunction() {
+  createHash(algorithm).update(data).digest('hex');
+}
+
+bench('crypto.hash', async () => {
+  await hashFunction();
+});
+
+bench('crypto.createHash', () => {
+  createHashFunction();
+});
+
+await run();
