4f81ddd58e0218a091ed7449db0ac9726e9aa43b
VDovydenkov <dov@mlbox.ru>
2025-04-05 22:59:33 +0300

    Clear the repository.

--- tasks_2024/task2_2024-11-06/cramerTest.mjs
+++
@@ -1,5 +0,0 @@
-﻿import { execSync } from 'node:child_process';
-
-const result = String(execSync('node cramer_v2.js 2 3 18 4 -5 -8')).trim().replace('/\n/g', ' ');
-
-console.assert(result === '3 4', 'НЕУДАЧА!');
--- tasks_2024/task4_2024-11-20/async_func.js
+++
@@ -1,5 +0,0 @@
-﻿async function task(x) {
-    let result = await x.name;
-    result = String(result) + '465778';
-    return Promise.resolve(result);
-}
